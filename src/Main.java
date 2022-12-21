3820ba26ab07804a75d38d281d75abe089abc229
ChrisDeSimplon <christophe.delapierre.simplon@gmail.com>
2022-12-21 16:43:19 +0100

    premier commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static int num=14;
+    public static void main(String[] args) {
+
+            System.out.println("quelques valeurs de variables primitives:");
+            boolean isTrue= true;
+            System.out.println(isTrue);
+            byte numeric =-128;
+            System.out.println(numeric);
+            char letter= 'Z';
+            System.out.println("lettre Z majuscule:  "+letter);
+            short petitnombre=61;
+            String mot="Toto";
+            System.out.println(mot);
+            int entier=9;
+            System.out.println(entier);
+            long grandNombre=200000000;
+            float pi=3.14159f;
+            double virgule=2.356d;
+            System.out.println("nombre flottant:  "+virgule);
+
+
+
+
+    }
+}
